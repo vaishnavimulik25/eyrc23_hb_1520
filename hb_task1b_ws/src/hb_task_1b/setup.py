55100be4facd9a29032f53a5dc8d28ba45fa54ba
jiten-topiwala <jitentopiwala7@gmail.com>
2023-10-10 13:01:30 +0530

    gazebo chalu 1st time

--- hb_task1b_ws/src/hb_task_1b/setup.py
+++ hb_task1b_ws/src/hb_task_1b/setup.py
@@ -1,16 +1,46 @@
 from setuptools import find_packages, setup
-
+from glob import glob
+import os
 package_name = 'hb_task_1b'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages',
+    #         ['resource/' + package_name]),
+    #     ('share/' + package_name, ['package.xml']),
+    #     ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    #     ('share/' + package_name + '/urdf', glob('urdf/*.xacro')),
+    #     ('share/' + package_name + '/meshes', glob('meshes/*')),
+    #     ('share/' + package_name + '/config', glob('config/*')),
+    #     ('share/' + package_name + '/worlds', glob('worlds/*')),
+    # ],
+    #discussion foroum
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages',
+    #         ['resource/' + package_name]),
+    #     ('share/' + package_name, ['package.xml']),
+    #     ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    #     ('share/' + package_name + '/urdf', glob('urdf/*.xacro')),
+    #     ('share/' + package_name + '/meshes', glob('meshes/*')),
+    #     ('share/' + package_name + '/worlds', glob('worlds/*')),
+    # ],
+    
+    #eyrc website
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+('share/ament_index/resource_index/packages',
+['resource/' + package_name]),
+('share/' + package_name, ['package.xml']),
+(os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+(os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+(os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+(os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+(os.path.join('share', package_name, 'config'), glob('config/*')),
+(os.path.join('share', package_name, 'worlds'), glob('worlds/*'))
+],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='g10',
