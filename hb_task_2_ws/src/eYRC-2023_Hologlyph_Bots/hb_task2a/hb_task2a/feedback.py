cf0e8a1d9d90edd17a8523d28bbfd252320bc1d1
vaishnavimulik25@ <vaishnavimulik25@gmail.com>
2023-11-04 12:00:19 +0530

    tried to remove dict error but spawn entity not responding

--- hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/hb_task2a/feedback.py
+++ hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/hb_task2a/feedback.py
@@ -35,17 +35,50 @@ class ArUcoDetector(Node,CvBridge):
         self.camera_subscriber = self.create_subscription(Image,"/camera/image_raw",self.image_callback,10)
 
     def image_callback(self, msg):
+        
         #convert ROS image to opencv image
         cvb = CvBridge()
         cv_image = cvb.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         self.get_logger().info("cv image converted")        
+
         #Detect Aruco marker
+        ap = argparser.ArgumentParser()
+        ap.add_argument("-i","--image",required=True,help="path to input image containing Aruco tag")
+        ap.add_argument("-t","--type",type=str,default="DICT_ARUCO_ORIGINAL",help="type of Aruco tag to detect")
+        args = vars(ap.parse_args())
+        # define names of each possible ArUco tag OpenCV supports
+        ARUCO_DICT = {
+        	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+        	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+        	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+        	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+        	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+        	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+        	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+        	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+        	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+        	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+        	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+        	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+        	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+        	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+        	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+        	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+        	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+        	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+        	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+        	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+        	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+        }
         arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
         arucoParams = cv2.aruco.DetectorParameters_create()
         (corners,ids,rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
         
+        self.get_logger().info("aruco detected")        
         # Publish the bot coordinates to the topic  /detected_aruco
-#        self.aruco_publisher = self.create_publisher(Pose)
+        #self.aruco_publisher = self.create_publisher(Pose)
+
+
 def main(args=None):
     rclpy.init(args=args)
 
