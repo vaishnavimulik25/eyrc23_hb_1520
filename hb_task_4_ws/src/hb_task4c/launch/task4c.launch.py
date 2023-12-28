c673c0503e0f56866518b0f37b7bcf6c5b244da8
Vaishnavi Mulik <vaishnavimulik25@gmail.com>
2023-12-28 18:06:15 +0530

    hbtask4c updated

--- hb_task_4_ws/src/hb_task4c/launch/task4c.launch.py
+++ hb_task_4_ws/src/hb_task4c/launch/task4c.launch.py
@@ -1,3 +1,21 @@
+''' 
+*****************************************************************************************
+*
+*        =============================================
+*                  HB Theme (eYRC 2023-24)
+*        =============================================
+*
+*
+*  Filename:			Spawn_bot.launch.py
+*  Description:         Use this file to spawn bot.
+*  Created:				16/07/2023
+*  Last Modified:	    16/09/2023
+*  Modified by:         Srivenkateshwar
+*  Author:				e-Yantra Team
+*  
+*****************************************************************************************
+'''
+
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
@@ -7,57 +25,39 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution,LaunchConfiguration, PythonExpression
 import os
 from ament_index_python.packages import get_package_share_directory,get_package_prefix
-from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess,
-                            LogInfo, RegisterEventHandler, TimerAction)
-from launch.conditions import IfCondition
-from launch.event_handlers import (OnExecutionComplete, OnProcessExit,
-                                OnProcessIO, OnProcessStart, OnShutdown)
-from launch.events import Shutdown
-from launch.substitutions import (EnvironmentVariable, FindExecutable,
-                                LaunchConfiguration, LocalSubstitution,
-                                PythonExpression)
+
 
 def generate_launch_description():
-
-
-    Publisher_node = Node(
+    share_dir = get_package_share_directory('hb_task4c')
+     
+    feedback_node = Node(
         package = "hb_task4c",
-        executable = "publisher"
+        executable = "feedback"
     )
-    run_agent = ExecuteProcess(
-    cmd=[[
-        'ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888',
 
-    ]],
-    shell=True
-)
-    
+    Controller_node1 = Node(
+        package="hb_task4c",
+        executable="controller1"
+    )
+
+    Controller_node2 = Node(
+        package="hb_task4c",
+        executable="controller2"
+    )
+
+    Controller_node3 = Node(
+        package="hb_task4c",
+        executable="controller3"
+    )
+
+    Next_Goal = Node(
+        package="hb_task4c",
+        executable="nextgoalpub"
+    )
     return LaunchDescription([
-        Publisher_node,
-        run_agent,
-        RegisterEventHandler(
-            OnProcessStart(
-                target_action=Publisher_node,
-                on_start=[
-                    LogInfo(msg='Publishing info'),
-                ]
-            )
-        ),
-        RegisterEventHandler(
-            OnProcessStart(
-                target_action=run_agent,
-                on_start=[
-                    LogInfo(msg='run_agent started'),
-                ]
-            )
-        ),
-        RegisterEventHandler(
-   	     OnShutdown(
-        	on_shutdown=[LogInfo(
-      	        	msg=['Launch was asked to shutdown: ',
-           	     	LocalSubstitution('event.reason')]
-                )]
-    	     )
-	),
-
+        feedback_node,
+        Controller_node1,
+        Controller_node2,
+        Controller_node3,
+        Next_Goal
         ])
--- hb_task_4_ws/src/hb_task4c/setup.py
+++ hb_task_4_ws/src/hb_task4c/setup.py
@@ -23,7 +23,11 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                    'publisher = hb_task4c.publisher:main'
+                    'feedback = hb_task4c.feedback:main',
+                    'nextgoalpub = hb_task2b.nextGoalPub:main',
+                    'controller1 = hb_task4c.bot_controller1:main',
+                    'controller2 = hb_task4c.bot_controller2:main',
+                    'controller3 = hb_task4c.bot_controller3:main'
         ],
     },
 )
