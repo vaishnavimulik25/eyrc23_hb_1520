30f6b480f8d1366fed1994f098972a06f6a38717
Vaishnavi Mulik <vaishnavimulik25@gmail.com>
2023-12-31 07:40:46 +0530

    launch file updated

--- hb_task_4_ws/src/hb_task4c/launch/task4c.launch.py
+++ hb_task_4_ws/src/hb_task4c/launch/task4c.launch.py
@@ -25,11 +25,35 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution,LaunchConfiguration, PythonExpression
 import os
 from ament_index_python.packages import get_package_share_directory,get_package_prefix
+from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess, LogInfo, RegisterEventHandler, TimerAction)
 
 
 def generate_launch_description():
     share_dir = get_package_share_directory('hb_task4c')
-     
+    
+    run_agent = ExecuteProcess(                                                
+    cmd=[[                                                                     
+        'ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888',           
+                                                                               
+    ]],                                                                        
+    shell=True
+    )
+
+    camera = ExecuteProcess(                                                
+    cmd=[[                                                                     
+        'ros2 launch usb_cam camera.launch.py'
+    ]],                                                                        
+    shell=True
+    )
+
+    calibrator = ExecuteProcess(                                                
+    cmd=[[                                                                     
+        'ros2 run camera_calibration cameracalibrator --size 8x6 --square 0.02 --ros-args -r image:=/camera1/image_raw -p camera:=/camera1'
+
+    ]],                                                                        
+    shell=True
+    )
+
     feedback_node = Node(
         package = "hb_task4c",
         executable = "feedback"
@@ -55,6 +79,9 @@ def generate_launch_description():
         executable="nextgoalpub"
     )
     return LaunchDescription([
+        run_agent,
+        camera,
+        calibrator,
         feedback_node,
         Controller_node1,
         Controller_node2,
